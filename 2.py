585cbf69b120281f2374c86eacdb74c25f058641
zakhart2 <zakhar.tkach1@mail.ru>
2024-06-09 14:39:23 +0300

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,22 @@
+name = 'Zakhar'
+first = 1
+second = 2
+print(len(name))
+summa = 3
+diff = -1
+print(summa, diff)
+first = 1
+second = 4
+third = 7
+mean = 4
+print(mean)
+first_string = 'вторник'
+second_string = 'Понедельник'
+print(second_string + ", " ,first_string)
+a = 2
+b = 3
+c = 4
+print(a * b + a * c)
+f = 14
+print(f**3)
+print(2744//2)
